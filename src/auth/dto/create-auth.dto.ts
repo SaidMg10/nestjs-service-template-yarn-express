7d87c9f8454ff30c737627e915f6e225e1fdcd87
Said Mendoza <ticcitoby777crepis@gmail.com>
2025-01-25 22:55:17 -0500

    feat: add CreateAuthDto for handling user authentication with password or googleId

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,22 @@
-export class CreateAuthDto {}
+import {
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateAuthDto {
+  @IsString()
+  @MinLength(6)
+  @MaxLength(50)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password must have uppercase, lowercase letters, and a number',
+  })
+  @IsOptional()
+  password?: string;
+
+  @IsString()
+  @IsOptional()
+  googleId?: string;
+}
